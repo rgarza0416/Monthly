ca3b29a30456ee537d5847e0c2213c85981eaaa2
Ricardo Garza <rgarza0416@yahoo.com>
2024-05-14 12:30:30 -0500

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Ricardo Garza on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
